23e1d07e27e50df0d0c71298edd2fb363085db3b
DavidEGiraldo <degiraldod@gmail.com>
2022-12-06 13:45:25 -0500

    routes changes

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ let cors = require("cors");
 let bodyParser = require("body-parser");
 require("dotenv").config();
 
-const orderRoute = require("../routes/order.route");
-const userRoute = require("../routes/user.route");
+const orderRoute = require("./routes/order.route");
+const userRoute = require("./routes/user.route");
 
-require("../db/db");
+require("./db/db");
 
 const app = express();
 app.use(bodyParser.json());
